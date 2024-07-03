36efb6e1229a3ac649486c39c8e08cd1e62034ea
Alash Shrestha <alashsth2019@gmail.com>
2024-07-03 22:08:24 +0545

    Fix user login

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,20 +4,24 @@ from .models import Food, Consume
 
 
 def index(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            food_consumed = request.POST['food_consumed']
+            consume = Food.objects.get(name=food_consumed)
+            user = request.user
+            consume = Consume(user=user, food_consumed=consume)
+            consume.save()
+            foods = Food.objects.all()
 
-    if request.method == "POST":
-        food_consumed = request.POST['food_consumed']
-        consume = Food.objects.get(name=food_consumed)
-        user = request.user
-        consume = Consume(user=user, food_consumed=consume)
-        consume.save()
-        foods = Food.objects.all()
+        else:
+            foods = Food.objects.all()
+        consumed_food = Consume.objects.filter(user=request.user)
 
+        return render(request, 'myapp/index.html', {'foods': foods, 'consumed_food': consumed_food})
     else:
         foods = Food.objects.all()
-    consumed_food = Consume.objects.filter(user=request.user)
-
-    return render(request, 'myapp/index.html', {'foods': foods, 'consumed_food': consumed_food})
+        consumed_food = None 
+        return render(request, "myapp/index.html", {'foods': foods, 'consumed_food': consumed_food})
 
 
 def delete_consume(request, id):
